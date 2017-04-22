7a32a9eac45faeaca0161640647d617a4fc09976
Sean Naylor <Sean Naylor>
2017-04-22 08:26:55 -0700

    whaaaaaa

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 var friends = require('../data/friends');
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
@@ -7,21 +8,52 @@ router.use(function timeLog(req, res, next) {
     next()
 });
 
-router.get("/friends", function(req, res) {
+//route for api link
+router.get("/friends", function (req, res) {
     res.json(friends);
 });
 
 
-// Create New Characters - takes in JSON input
+// Create new friend - takes in JSON input
 router.post("/friends", function (req, res) {
+
     var newFriend = req.body;
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+    var newFriendScore = newFriend.score;
+    var scoreDiff = 0;
 
-    console.log(newFriend);
 
-    friends.push(newFriend);
+    var bestMatch = {
+        name: "",
+        image: "",
+        matchDiff: ""
+    }
 
-    res.json(newFriend);
+    //loop trhough friends array
+    for (var i = 0; i < friends.length; i++) {
+
+        scoreDiff = 0;
+
+        //for each friend check difference in user score against current index
+        for (var n = 0; n < 10; n++) {
+
+            scoreDiff += Math.abs(parseInt(newFriendScore[n]) - parseInt(friends[i].score[n]));
+
+            if (scoreDiff <= bestMatch.matchDiff) {
+                bestMatch.name = friends[i].name;
+                bestMatch.photo = friends[i].photo;
+                bestMatch.matchDiff = scoreDiff;
+            }
+
+        }
+
+    }
+
+    fs.appendFile('friends.js', newFriend, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
+    res.json(bestMatch);
 });
 
 module.exports = router;
